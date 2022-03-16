f35a092ba1b08948351e82403dbe9ce278a346fa
Evie <evie.ball32@gmail.com>
2022-03-16 17:10:58 +0000

    url endings

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.listen(PORT, () => {
 
 
 
-/* const flower = [
+const flower = [
   {title: "Flower Delivery", url: "https://www.interflora.co.uk/" },
   {title: 'Flower - Wiktionary', url: `https://en.wikipedia.org/wiki/Flower` },
   {title: 'LED Flower Bouquet', url: `https://hackaday.com/2022/03/15/led-flower-bouquet-is-a-radiant-hacker-desk-decoration/` },
@@ -23,39 +23,24 @@ app.listen(PORT, () => {
 ]
 
 const bulb = [
-  {title:" Bulb | The energy company" url: "https://bulb.co.uk/"},
-  {title:" Bulb - Facebook | Home" url: "https://www.facebook.com/bulb/"},
-  {title:" Bulb | LinkedIn" url: "https://www.linkedin.com/company/bulb-uk"},
-  {title:" Buy flower bulbs with Avon Bulbs" url: "https://www.avonbulbs.co.uk/"},
-  {title:" Plants, seeds & Bulbs" url: "https://www.diy.com/departments/outdoor-garden/plants-seeds-bulbs/bulbs/DIY581228.cat"},
-  {title:" Bulbasaur" url: "https://next-pokedex.netlify.app/1"},
-  {title:" Bulbasaurus" url: "https://bulbasaurus.netlify.app"},
+  {title:" Bulb | The energy company", url: "https://bulb.co.uk/"},
+  {title:" Bulb - Facebook | Home", url: "https://www.facebook.com/bulb/"},
+  {title:" Bulb | LinkedIn", url: "https://www.linkedin.com/company/bulb-uk"},
+  {title:" Buy flower bulbs with Avon Bulbs", url: "https://www.avonbulbs.co.uk/"},
+  {title:" Plants, seeds & Bulbs", url: "https://www.diy.com/departments/outdoor-garden/plants-seeds-bulbs/bulbs/DIY581228.cat"},
+  {title:" Bulbasaur", url: "https://next-pokedex.netlify.app/1"},
+  {title:" Bulbasaurus", url: "https://bulbasaurus.netlify.app"},
 
 ]
 
 const root = [
-  {title:"Root | Board Game | BoardGameGeek" url: "https://boardgamegeek.com/boardgame/237182/root"},
-  {title:"Root - Wikipedia" url: "Root - Wikipedia"},
-  {title:"" url: ""},
-  {title:" Root Definition & Meaning - Merriam-Webster" url: "https://www.merriam-webster.com/dictionary/root"},
-  {title:" Root - Dire Wolf Digital" url: "https://www.direwolfdigital.com/root/"},
-] */
+  {title:"Root | Board Game | BoardGameGeek", url: "https://boardgamegeek.com/boardgame/237182/root"},
+  {title:"Root - Wikipedia", url: "Root - Wikipedia"},
+  {title:" Root Definition & Meaning - Merriam-Webster", url: "https://www.merriam-webster.com/dictionary/root"},
+  {title:" Root - Dire Wolf Digital", url: "https://www.direwolfdigital.com/root/"},
+]
 
-queries = [ "flower", "bulb", "root" ]
-
-function search(entry) {
-  if (entry.lower in queries) {
-
-  }
-
-}
-
-app.get('/search/:something', (req, res) => {
-  console.log(req.params)
-  const entry = req.params.something
-  const query = queries.find(query => query.toLowerCase() === entry.toLowerCase())
-  res.json(query)
-})
+queries = [ flower, bulb, root ]
 
 app.get('/', (req, res) => res.send('What have you argued with your friends about and finally decided to schmoogle?'))
 
@@ -63,9 +48,23 @@ app.get('/data', (req, res) => {
   res.json(queries)
 })
 
-app.get('/quotes/:entry', (req, res) => {
-  const entry = (req.params);
+app.get('/lucky_search', (req, res) => {
+  let index = Math.floor(Math.random() * queries.length);
+  res.json(queries[index])
+})
 
+app.get('/random', (req, res) => {
+  let index = Math.floor(Math.random() * queries.length);
+  let result = queries[index]
+  let superindex = Math.floor(Math.random() * result.length)
+  res.json(result[superindex])
+})
+
+app.get('/search/:something', (req, res) => {
+  //console.log(req.params)
+  const entry = req.params.something
+  const query = queries.find(query => query.toLowerCase() === entry.toLowerCase()) 
+  res.json(query)
 })
 
 
